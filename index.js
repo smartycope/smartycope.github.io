6b7517e9f942cbd482e70018bb8b7076e8904295
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2024-06-13 00:53:35 +0000

    Built site for gh-pages

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// // This is the only way I can figure out so far to customize CSS. It works for now.
-// const links = document.querySelector('.quarto-other-links')
-// links.style.marginTop = '25px'
-// const other_links = document.querySelector('.quarto-other-links > h2')
-// other_links.innerText = 'Quick Links'
-// other_links.style.fontSize = 'large'
+// This is the only way I can figure out so far to customize CSS. It works for now.
+const links = document.querySelector('.quarto-other-links')
+links.style.marginTop = '25px'
+const other_links = document.querySelector('.quarto-other-links > h2')
+other_links.innerText = 'Quick Links'
+other_links.style.fontSize = 'large'
 
-// const titles = [
-//     '.quarto-other-links > ul > li:nth-child(4)',
-//     '.quarto-other-links > ul > li:nth-child(7)',
-//     '.quarto-other-links > ul > li:nth-child(10)',
-// ]
-// for (const i of titles){
-//     const elem = document.querySelector(i)
-//     elem.style.marginTop = '10px'
-//     elem.style.fontSize = 'large'
-// }
+const titles = [
+    '.quarto-other-links > ul > li:nth-child(4)',
+    '.quarto-other-links > ul > li:nth-child(7)',
+    '.quarto-other-links > ul > li:nth-child(10)',
+]
+for (const i of titles){
+    const elem = document.querySelector(i)
+    elem.style.marginTop = '10px'
+    elem.style.fontSize = 'large'
+}
